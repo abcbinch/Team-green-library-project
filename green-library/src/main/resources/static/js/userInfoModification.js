c39d90fd5b566594f8c8b3a27eb2116d8b061cf5
qwerty645798 <harry980523@naver.com>
2024-07-04 15:46:44 +0900

    harry

--- green-library/src/main/resources/static/js/userInfoModification.js
+++ green-library/src/main/resources/static/js/userInfoModification.js
@@ -5,9 +5,10 @@ const email = document.getElementById('email');
 const mailspace = document.getElementById('email2');
 const eselect = document.getElementById('emailSel');
 
-const pswdRegexp = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#^*_]).{8,20}$/;
+const pswdRegexp = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#^*_])[A-Za-z\d!@#^*_]{8,20}$/;
 
 form.addEventListener('submit', function(e) {
+	console.log(1);
 	concatInput();
 	if (!modifyCheck()) {
 		e.preventDefault();
@@ -15,35 +16,37 @@ form.addEventListener('submit', function(e) {
 });
 
 function modifyCheck() {
+	console.log(2);
 	if (!pswd.value) {
 		alert("비밀번호를 입력해주세요.");
 		pswd.focus();
 		return false;
 	}
+	console.log(1);
 	if (pswd.value.length < 8 || pswd.value.length > 20) {
 		alert("비밀번호는 8자에서 20자 사이여야 합니다.");
 		pswd.focus();
 		return false;
 	}
 	if (!pswdRegexp.test(pswd.value)) {
-		alert("비밀번호는 숫자, 영문 대소문자, 특수문자 !, @, #, ^, *, _ 중 3가지 이상 조합이어야 합니다.");
+		alert("비밀번호는 숫자, 영문 대소문자, 특수문자 !, @, #, ^, *, _ 를 모두 포함한 조합이어야 합니다.");
 		pswd.focus();
 		return false;
-	}
+	}console.log(1);
 	if (pswd.value !== passCheck.value || !passCheck.value) {
 		alert("비밀번호가 틀렸습니다.");
 		passCheck.focus();
 		return false;
-	}
+	}console.log(1);
 	if (!email.value) {
 		alert("이메일을 입력해주세요.");
 		email.focus();
 		return false;
-	}
+	}console.log(1);
 	if (!mailspace.value) {
 		alert("이메일 주소를 선택해주세요.");
 		return false;
-	}
+	}console.log(1);
 	alert("회원정보가 수정되었습니다.");
 	return true;
 }
